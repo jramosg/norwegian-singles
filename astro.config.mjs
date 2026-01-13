00c1977877210475831b0805a4f7326348d89a7a
Jon <jonurnieta@gmail.com>
2026-01-13 10:28:52 +0100

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,23 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://norwegian-singles.app',
-  integrations: [react()],
+  integrations: [
+    react(),
+    sitemap({
+      i18n: {
+        defaultLocale: 'en',
+        locales: {
+          en: 'en-US',
+          es: 'es-ES',
+        },
+      },
+    }),
+  ],
   i18n: {
     defaultLocale: 'es',
     locales: ['es', 'en'],
